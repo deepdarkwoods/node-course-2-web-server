2abb0998a798187af5952c18c54cccb64dcd46dc
howdy dudy <john@nothing.com>
2017-10-11 06:37:38 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app  = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+
+app.use((req,res,next) =>{
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  fs.appendFile('server.log',log + '\n',(err)=>{
+    if (err){
+      Console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next)=>{
+
+//   res.render('maintenance.hbs');
+
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    currentYear:new Date().getFullYear(),
+    welcomeMsg:'Fuck off you cunt'
+  });
+
+});
+
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'Welcome Page',
+        currentYear:new Date().getFullYear()
+  
+    });
+});
+
+
+app.listen(3000);
+
